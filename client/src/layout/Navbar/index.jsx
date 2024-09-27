7ed0d63bc264dbd6bdf42dded9e0cf356230f846
AliIsmayl <ali.ismayil.681@gmail.com>
2024-09-27 14:48:27 +0400

    some update

--- client/src/layout/Navbar/index.jsx
+++ client/src/layout/Navbar/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import './navbar.scss'
+import React, { useState } from "react";
+import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { IoMenu } from "react-icons/io5";
@@ -9,13 +9,14 @@ import { IoLogoInstagram } from "react-icons/io";
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
-import { Link as ScrollLink } from 'react-scroll';
+import { Link as ScrollLink } from "react-scroll";
+import Logo from "../../image/Logo.png";
 
 function Navbar() {
-  const [openNavbar, setOpenNavbar] = useState(false)
+  const [openNavbar, setOpenNavbar] = useState(false);
 
   function handleOpennavbar() {
-    setOpenNavbar(!openNavbar)
+    setOpenNavbar(!openNavbar);
   }
   // const getElementPosition = (id) => {
   //   const element = document.getElementById(id);
@@ -35,83 +36,61 @@ function Navbar() {
   //   }
   // };
 
-
-
-
   return (
     <nav>
       <div className="navbarLeftBox">
         <div className="iconBox">
-          <img src="https://png.pngtree.com/png-vector/20220816/ourmid/pngtree-law-firm-logo-and-icon-design-template-vector-silhouette-judge-abstract-vector-png-image_38449268.png" alt="" />
+          <img
+            src={Logo}
+            alt=""
+          />
         </div>
         <div className="textBox">
           <ul>
             <ScrollLink to="Home" smooth={true} duration={500} offset={-50}>
-              <li>
-                Home
-              </li>
+              <li>Home</li>
             </ScrollLink>
             <ScrollLink to="About" smooth={true} duration={500} offset={-50}>
-              <li>
-                About
-              </li>
+              <li>About</li>
             </ScrollLink>
             <ScrollLink to="Service" smooth={true} duration={500} offset={-50}>
-              <li>
-                Service
-              </li>
+              <li>Service</li>
             </ScrollLink>
             <ScrollLink to="News" smooth={true} duration={500} offset={-50}>
-              <li>
-                News
-              </li>
+              <li>News</li>
             </ScrollLink>
             <ScrollLink to="Contact" smooth={true} duration={500} offset={-50}>
-              <li>
-                Contact
-              </li>
+              <li>Contact</li>
             </ScrollLink>
           </ul>
-
-
         </div>
         <div className="menuBox" onClick={handleOpennavbar}>
           <IoMenu />
         </div>
       </div>
       <div className="navbarRightBox">
-        <FaPhoneVolume style={{ fontSize: '40px' }} />
+        <FaPhoneVolume style={{ fontSize: "40px" }} />
         <h1>+994 50 593 18 38</h1>
       </div>
-      <div className={`respNavbar ${openNavbar ? 'openNavbar' : ""}`}>
+      <div className={`respNavbar ${openNavbar ? "openNavbar" : ""}`}>
         <ul>
           <ScrollLink to="Home" smooth={true} duration={500} offset={-50}>
-            <li onClick={handleOpennavbar}>
-              Home
-            </li>
+            <li onClick={handleOpennavbar}>Home</li>
           </ScrollLink>
           <ScrollLink to="About" smooth={true} duration={500} offset={-50}>
-            <li onClick={handleOpennavbar}>
-              About
-            </li>
+            <li onClick={handleOpennavbar}>About</li>
           </ScrollLink>
           <ScrollLink to="Service" smooth={true} duration={500} offset={-50}>
-            <li onClick={handleOpennavbar}>
-              Service
-            </li>
+            <li onClick={handleOpennavbar}>Service</li>
           </ScrollLink>
           <ScrollLink to="News" smooth={true} duration={500} offset={-50}>
-            <li onClick={handleOpennavbar}>
-              News
-            </li>
+            <li onClick={handleOpennavbar}>News</li>
           </ScrollLink>
           <ScrollLink to="Contact" smooth={true} duration={500} offset={-50}>
-            <li onClick={handleOpennavbar}>
-              Contact
-            </li>
+            <li onClick={handleOpennavbar}>Contact</li>
           </ScrollLink>
         </ul>
-        <div className='iconsBox'>
+        <div className="iconsBox">
           <div className="closeBtn" onClick={handleOpennavbar}>
             <IoMdClose />
           </div>
@@ -133,7 +112,7 @@ function Navbar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
